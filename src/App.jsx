2cbad9a4f94c628d84911e2b95bd2d8668e63833
haydulski <damian.hajdukiewicz@gmail.com>
2023-04-28 16:53:24 +0200

    add picture descriptions and loading indicator

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import { useEffect } from 'react'
 import Configurator from './Configurator'
+import NProgress from 'nprogress';
 
 function App() {
+
+  //loading indicator
+  useEffect(() => {
+    NProgress.set(0.0);
+    return () => { NProgress.set(1.0) }
+  }, [])
 
   return (
     <>
